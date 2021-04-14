7e8d7532c6ae39b263f8a3cfc72ce8428763e0df
muhamadalfarisy98 <muhamadalfarisy9d@yahoo.com>
2021-04-14 11:14:58 +0700

    [WIP] add manifest line

--- asb_travel_umroh/models/paket_perjalanan.py
+++ asb_travel_umroh/models/paket_perjalanan.py
@@ -21,7 +21,7 @@ class PaketPerjalanan(models.Model):
     quota = fields.Integer(string='Quota')
     remaining_seats = fields.Integer(string='Remaining Seats',
         compute='_get_remaining_seats',store=True,readonly=True)
-    quota_progress = fields.Float(string='Quota Progress')
+    quota_progress = fields.Float(string='Quota Progress',compute='_get_quota_progress')
     state = fields.Selection(string='Status', selection=[
         ('draft', 'Draft'), ('confirmed', 'Confirmed'),
         ('done', 'Done')],default='draft')
@@ -32,6 +32,8 @@ class PaketPerjalanan(models.Model):
     paket_acara_line = fields.One2many(comodel_name='paket.acara.line', inverse_name='paket_perjalanan_id', 
         string='Schedule Lines')
     hpp_line = fields.One2many(comodel_name='hpp.line', inverse_name='paket_perjalanan_id', string='hpp Lines')
+    paket_peserta_line = fields.One2many(comodel_name='paket.peserta.line', inverse_name='paket_perjalanan_id', 
+        string='Manifest')
     
     note= fields.Text(string='Note')
     amount_total = fields.Monetary(string='Total', store=True, readonly=True, 
@@ -49,19 +51,28 @@ class PaketPerjalanan(models.Model):
         result = super(PaketPerjalanan, self).create(vals)
         return result
     
-    @api.depends('paket_pesawat_line','quota')
+    @api.depends('paket_peserta_line','quota')
     def _get_remaining_seats(self):
         for r in self:
             if not r.quota:
                 r.remaining_seats=0
             else:
-                r.remaining_seats=r.quota-len(r.paket_pesawat_line)
+                r.remaining_seats=r.quota-len(r.paket_peserta_line)
+
+    @api.depends('paket_peserta_line','quota')
+    def _get_quota_progress(self):
+        for r in self:
+            if not r.quota:
+                r.quota_progress=0
+            else:
+                r.quota_progress=(len(r.paket_peserta_line)/ r.quota)*100
+
 
     @api.onchange('quota')
     def _onchange_quota(self):
         if self.quota<0:
             raise ValidationError('Tidak bisa kurang dari 0')
-        if self.quota<len(self.paket_pesawat_line):
+        if self.quota<len(self.paket_peserta_line):
             raise ValidationError("Increase min Quota or remove excess Jamaah")
     
     @api.onchange('product_tmpl_id')
@@ -106,6 +117,7 @@ class PaketPerjalanan(models.Model):
             r.write({'state': 'done'})
 
     def button_update_jamaah(self):
+        '''Method to update manifest line by searching SO with same paket perjalanan id'''
         for r in self:
             return True
 
@@ -167,6 +179,36 @@ class HppLine(models.Model):
         if self.price_subtotal<0:
             raise ValidationError('Tidak bisa kurang dari 0')
     
+class PaketPesertaLine(models.Model):
+    _name = 'paket.peserta.line'
+    _description = 'Paket Peserta Line'
+    _inherits = {'res.partner': 'jamaah_id'}
+
+    paket_perjalanan_id = fields.Many2one(comodel_name='paket.perjalanan', string='Paket perjalanan')
+    jamaah_id = fields.Many2one(comodel_name='res.partner', string='Jamaah',
+        domain="[('is_customer', '=', True)]")
+    # Personal
+    # ## related
+    
+    # no_ktp = fields.Char(string='KTP No',related='jamaah_id.no_ktp',readonly=True)
+    # birthdate = fields.Date(string='Date of Birth',related='jamaah_id.birthdate',readonly=True)
+    # place_of_birth = fields.Char(string='Place of Birth',related='jamaah_id.place_of_birth',readonly=True)
+    # title_id = fields.Many2one(comodel_name='res.partner.title', string='Title',related='jamaah_id.title',readonly=True)
+    # non-related
+    room_type = fields.Selection(string='Room Type', help='Paket kamar',required=True,
+        selection=[('quad', 'Quad'), ('double', 'Double'),('triple', 'Triple')],default='quad')
+    # age = fields.Integer(string='Age',compute='_get_age_jamaah',store=True)
+    # mahram_ids = fields.Many2many(comodel_name='res.partner', string='Mahram',domain="[('is_customer', '=', True)]")
+    
+    # Passport
+    #Passport information
+    # no_passport = fields.Char(string='Passport No',related='jamaah_id.no_passport',readonly=True)
+    # date_expired = fields.Date(string='Date of Expired',readonly=True)
+    # date_issued = fields.Date(string='Date Issued',readonly=True)
+    # name_passport = fields.Char(string='Passport Name',readonly=True)
+    # imigrasi = fields.Char(string='Imigrasi',readonly=True)
+    note = fields.Text(string='Notes')
     
     
+
     
