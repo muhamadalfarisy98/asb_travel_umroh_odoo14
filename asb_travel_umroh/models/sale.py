5945314da355a5c288098f8f4bf0a5f29670b67c
muhamadalfarisy98 <muhamadalfarisy9d@yahoo.com>
2021-04-14 09:39:30 +0700

    [WIP] order_line

--- asb_travel_umroh/models/sale.py
+++ asb_travel_umroh/models/sale.py
@@ -7,3 +7,17 @@ class SaleOrder(models.Model):
     paket_perjalanan_id = fields.Many2one(comodel_name='paket.perjalanan', 
         string='Travel Package')
     
+    @api.onchange('paket_perjalanan_id')
+    def _onchange_product_tmpl_id(self):
+        for r in self:
+            if r.paket_perjalanan_id:
+                # removes all existing (previous) value from list
+                lines=[(5,0,0)]
+                line=self.paket_perjalanan_id.product_id
+                vals={
+                        'product_id':line.id,
+                        'name':line.name,
+                    }
+                lines.append((0,0,vals))
+                r.order_line=lines
+                
